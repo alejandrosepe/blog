25b5e66124e4d7f823ce777b9aa2676dfbadd2cc
Alejandro Sepe <77165173+alejandrosepe@users.noreply.github.com>
2023-11-23 21:36:08 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+// Ejemplo de funcionalidad (puedes personalizarlo según tus necesidades)
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('#myButton');
+    button.addEventListener('click', function() {
+        alert('¡Bienvenido al Barrio Cerrado XYZ!');
+    });
+});
