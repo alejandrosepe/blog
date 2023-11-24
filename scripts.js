2ae81f970fad65d4414cb945d85b2d44078fe969
Alejandro Sepe <77165173+alejandrosepe@users.noreply.github.com>
2023-11-23 22:08:37 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// scripts.js
-
-// Ejemplo de funcionalidad (puedes personalizarlo según tus necesidades)
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelector('#myButton');
-    button.addEventListener('click', function() {
-        alert('¡Bienvenido al Barrio Cerrado XYZ!');
-    });
-});
